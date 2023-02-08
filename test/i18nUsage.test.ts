a4914c02febc30d99024df921d8e5ba3e4e65d0f
hans774882968 <774882968@qq.com>
2023-02-08 22:58:45 +0800

    test: add test cases

--- test/i18nUsage.test.ts
+++ test/i18nUsage.test.ts
@@ -167,6 +167,48 @@ ruleTester.run('i18n-usage', rule, {
         { messageId: 'firstArgShouldBeString' }, { messageId: 'parameter' },
         { messageId: 'parameter' }, { messageId: 'firstArgShouldBeString' }
       ]
+    },
+    {
+      code: `
+        function f() {
+          const $i18n = () => {};
+          const $gt = () => {};
+          const $t = () => {};
+          console.log($gt());
+          console.log($t());
+          console.log($i18n());
+          const x = '1234';
+          console.log($gt(x));
+          console.log($t(x));
+          console.log($i18n(x));
+
+          console.log($gt('5678'));
+          console.log($t('5678'));
+          console.log($i18n('5678'));
+
+          console.log($gt(1234));
+          console.log($t(1234));
+          console.log($i18n(1234));
+
+          console.log($gt(true));
+          console.log($t(true));
+          console.log($i18n(true));
+
+          console.log($gt(12 + 34));
+          console.log($t(12 + 34));
+          console.log($i18n(12 + 34));
+        }
+
+        f();
+      `,
+      options: [
+        { i18nFunctionNames: ['$i18n'] }
+      ],
+      errors: [
+        { messageId: 'parameter' }, { messageId: 'firstArgShouldBeString' },
+        { messageId: 'firstArgShouldBeString' }, { messageId: 'firstArgShouldBeString' },
+        { messageId: 'firstArgShouldBeString' }
+      ]
     }
   ]
 });
